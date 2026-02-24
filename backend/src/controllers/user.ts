723ca44466f67d62462bdacaaa4a49feac5c04f7
NathanTheDev <nathan.smith1922@gmail.com>
2026-02-24 15:05:27 +1100

    feat: added /me route

--- backend/src/controllers/user.ts
+++ backend/src/controllers/user.ts
@@ -1,15 +1,34 @@
 
 import type { Request, Response } from "express";
+import { supabase } from "../util.js";
 
 export default class UserController {
     static me = async (req: Request, res: Response): Promise<void> => {
         try {
-            res.json({ message: "good!" });
-        } catch (error) {
-            console.error("Authentication error:", error);
-            res.status(500).json({ error: error });
-        }
-    };
-}
+            const { data, error } = await supabase
+                .schema("public")
+                .from("users")
+                .select()
+                .eq("id", req.user!.sub)
+                .maybeSingle();
 
+            if (error) {
+                const message = { error: error.message };
+                res.status(500).json(message);
+                return;
+            }
+
+            if (!data) {
+                const message = { error: "User profile not found" };
+                res.status(404).json(message);
+                return;
+            }
+
+            res.json(data)
+        } catch (err) {
+            console.log("I caught an error");
+            res.status(500).json({ message: "Internal server error" });
+    }
+  };
+}
 
