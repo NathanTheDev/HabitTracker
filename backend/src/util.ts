f0d5ee016bf9d68fd2f4f983f6b1e9338a7823d1
NathanTheDev <nathan.smith1922@gmail.com>
2026-03-11 13:51:36 +1100

    chore: removed old token verifier

--- backend/src/util.ts
+++ backend/src/util.ts
@@ -3,57 +3,7 @@ import type { Request, Response, NextFunction } from "express";
 import { createClient, SupabaseClient } from '@supabase/supabase-js';
 import { SUPABASE_SERVICE_ROLE_KEY, SUPABASE_URL, JWT_SECRET_KEY } from './config.js';
 
-export const authSupabase: SupabaseClient = createClient(
-    SUPABASE_URL,
-    SUPABASE_SERVICE_ROLE_KEY,
-    {
-        auth: {
-            autoRefreshToken: false,
-            persistSession: false
-        }
-    }
-);
-
 export const supabase: SupabaseClient = createClient(
     SUPABASE_URL,
     SUPABASE_SERVICE_ROLE_KEY,
-    {
-        auth: {
-            autoRefreshToken: false,
-            persistSession: false
-        }
-    }
 );
-
-export const extractToken = async (
-  req: Request,
-  res: Response,
-  next: NextFunction
-): Promise<void> => {
-  const authHeader = req.headers.authorization;
-  console.log("header", authHeader);
-
-  if (!authHeader || !authHeader.startsWith("Bearer ")) {
-    res.status(401).json({ error: "Unauthorized" });
-    return;
-  }
-
-  const token = authHeader.slice("Bearer ".length);
-
-  try {
-    const { data, error } = await supabase.auth.getUser(token);
-
-    if (error || !data.user) {
-      res.status(403).json({ error: "Invalid token" });
-      return;
-    }
-
-    req.user = data.user;
-
-    next();
-  } catch (err) {
-    console.error("Auth middleware error:", err);
-    res.status(500).json({ error: "Authentication failed" });
-  }
-};
-
