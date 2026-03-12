457fa301824b8334327d05deb8cb01d58e3fb7cc
NathanTheDev <nathan.smith1922@gmail.com>
2026-03-12 17:01:12 +1100

    feat: added routes for creating and patching habits

--- backend/src/controllers/user.ts
+++ backend/src/controllers/user.ts
@@ -1,5 +1,4 @@
 import { Request, Response } from "express";
-import { supabase } from "../util.js";
 
 export default class UserController {
   static async me(req: Request, res: Response) {
---
+++ backend/src/routes/habit.route.ts
@@ -0,0 +1,13 @@
+import { Router, Response } from "express";
+import { SessionRequest } from "supertokens-node/framework/express";
+import { requireAuth } from "../middleware/auth.js";
+import { supabase } from "../util.js";
+import HabitController from "../controllers/habit.js";
+
+const router = Router();
+
+router.post("/", requireAuth, HabitController.create);
+
+router.patch("/:id/complete", requireAuth, HabitController.patch);
+
+export default router;
