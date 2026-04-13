0b2b0a798354d5d38a2c5b4cef6f2534fe24d76e
NathanTheDev <nathan.smith1922@gmail.com>
2026-04-13 13:39:38 +1000

    feat: restarting

--- backend/src/controllers/user.ts
+++
@@ -1,7 +0,0 @@
-import { Request, Response } from "express";
-
-export default class UserController {
-  static async me(req: Request, res: Response) {
-    return res.json({ user: req.user });
-  }
-}
--- backend/src/util.ts
+++
@@ -1,9 +0,0 @@
-
-import type { Request, Response, NextFunction } from "express";
-import { createClient, SupabaseClient } from '@supabase/supabase-js';
-import { SUPABASE_SERVICE_ROLE_KEY, SUPABASE_URL, JWT_SECRET_KEY } from './config.js';
-
-export const supabase: SupabaseClient = createClient(
-    SUPABASE_URL,
-    SUPABASE_SERVICE_ROLE_KEY,
-);
